f072cbe4c957e6dd7c1f5bf9d667c3aee7769faa
Daniel J. Szelogowski <dszelogowski@gmail.com>
2022-09-09 18:38:34 -0500

    Update Test2.java

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,6 @@
 public class Test2 {
   public static void main(String[] args) {
     System.out.println("This is to make sure replit works still!");
+    System.out.println("This is another test to make sure pull requests work!");
   }
 }
