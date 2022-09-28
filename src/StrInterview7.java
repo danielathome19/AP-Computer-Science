27eb47e8c84c045e10b8c5a772297d4407be62cb
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-09-28 10:28:48 -0500

    Update

---
+++ src/StrInterview7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+// Daniel Szelogowski
+// 9/28/2022
+// String Interview Question 7
+public class StrInterview7 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String mytext = keyboard.nextLine();
+
+        int vowelcount = 0;
+        for (int lcv = 0; lcv < mytext.length(); lcv++) {
+            char cur = mytext.charAt(lcv);
+            if (cur == 'a' || cur == 'e' || cur == 'i' || cur == 'o' || cur == 'u')
+                vowelcount++;
+        }
+
+        int conscount = mytext.length() - vowelcount;
+        System.out.printf("# Vowels: %d\t\t# Consonants: %d\n", vowelcount, conscount);
+    }
+}
+/*
+Enter a string: beautiful
+# Vowels: 5		# Consonants: 4
+*/
