faa4b98d7f9c4af70779b3269b9a3b8f10c7bf79
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-10-03 10:12:13 -0500

    Update

---
+++ src/dfile.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class dfile {
+    public static void main(String[] args) {
+        try {  // On Replit, may need to put "../data/prog285b.dat"
+            Scanner input = new Scanner(new File("data/prog285b.dat"));
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
+}
