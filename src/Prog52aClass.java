4d73aa63ad0159cf42ffeb9be8d851314fed326d
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-10-17 10:28:31 -0500

    Update

---
+++ src/Prog52aClass.java
@@ -0,0 +1,28 @@
+// Daniel Szelogowski
+// Prog52aCl
+// 10/17/2022
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class Prog52aClass {
+    // Driver program
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+
+        out.print("Please enter length: ");
+        int len = kbd.nextInt();
+        out.print("Please enter width: ");
+        int wid = kbd.nextInt();
+        out.println();
+
+        // Make a new object (instance) of Cl52a (instantiate the class)
+        Cl52a wow = new Cl52a(len, wid);
+        wow.setStuff();
+
+        int area = wow.getArea();
+        int perim = wow.getPerim();
+
+        printer.println("Area: " + area);
+        printer.println("Perim: " + perim);
+    }
+}
---
+++ src/printer.java
@@ -0,0 +1,9 @@
+public class printer {
+    public static void println(Object arg) {
+        System.out.println(arg);
+    }
+
+    public static void print(Object arg) {
+        System.out.print(arg);
+    }
+}
