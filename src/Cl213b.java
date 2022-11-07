6141eeef91b255b65d9035365568fc92b2367647
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-11-07 10:15:23 -0600

    Update

---
+++ src/Cl213b.java
@@ -0,0 +1,26 @@
+// Daniel Szelogowski
+// Prog213b
+// 11/7/2015
+public class Cl213b {
+    private int myQuant;
+    private double myPrice;
+    private double myAmt;
+
+    public Cl213b(int quantity) {
+        myQuant = quantity;
+        myPrice = 0;
+        myAmt = 0;
+    }
+
+    public void calc() {
+        if (myQuant > 0 && myQuant < 100) myPrice = 5.95;
+        else if (myQuant > 99 && myQuant < 200) myPrice = 5.75;
+        else if (myQuant > 199 && myQuant < 300) myPrice = 5.40;
+        else if (myQuant >= 300) myPrice = 5.15;
+        myAmt = myPrice * myQuant;
+    }
+
+    public String toString() {
+        return "Price = $" + myPrice + "\tAmount Due = $" + String.format("%.2f", myAmt);
+    }
+}
--- src/dfile.java
+++ src/dfile.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class dfile {
     public static void main(String[] args) {
-        try {  // On Replit, may need to put "../data/prog285b.dat"
+        try {
             Scanner input = new Scanner(new File("data/prog285b.dat"));
 
         } catch (IOException e) {
