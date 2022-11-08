b88ca9aa1dcc76abdaadbed90e7eb9ae531ff541
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-11-08 10:13:41 -0600

    Update

---
+++ src/Cl213f.java
@@ -0,0 +1,20 @@
+// Daniel Szelogowski
+// Prog213f
+// 11/8/2022
+public class Cl213f {
+    private double myKwh;
+    private double myCost;
+
+    public Cl213f(double kwh) {
+        myKwh = kwh;
+        myCost = 0;
+    }
+
+    public void calc() {
+
+    }
+
+    public String toString() {
+        return "The number of ";
+    }
+}
