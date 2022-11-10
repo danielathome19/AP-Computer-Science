1a397797d2813f6f43233a94ce1f5372ea3eb025
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-11-10 10:22:42 -0600

    Update

---
+++ src/Cl435g.java
@@ -0,0 +1,17 @@
+// Daniel Szelogowski
+// Prog435g
+// 11/10/2022
+public class Cl435g {
+    private int myScore;
+    private int myRank;
+
+    public Cl435g(int score) {
+        myScore = score;
+        myRank = 0;
+    }
+
+    public int getScore() { return myScore; }
+    public int getRank() { return myRank; }
+    public void setRank(int rank) { myRank = rank; }
+    public String toString() { return myScore + "\t\t" + myRank; }
+}
