3673f8f9de6eda3401932210076d224524ec383e
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-11-21 10:28:46 -0600

    Update

--- src/NumberCube.java
+++ src/NumberCube.java
@@ -1,4 +1,6 @@
 public class NumberCube {
+    /** @return an integer value between 1 and 6, inclusive
+     */
     public int toss() {
         return (int)(Math.random() * 6) + 1;
     }
--- src/NumberCubeMain.java
+++ src/NumberCubeMain.java
@@ -1,6 +1,14 @@
 public class NumberCubeMain {
     private int notReady;
     private static int isReady = 18;
+
+    /** Returns an array of the values obtained by tossing
+     * a number cube numTosses times.
+     * @param cube a NumberCube
+     * @param numTosses the number of tosses to be recorded
+     * Precondition: numTosses > 0
+     * @return an array of numTosses values
+     */
     public static int[] getCubeTosses(NumberCube cube, int numTosses) {
         // Static means ready at compile-time
         // i.e., we don't have to make an object to use the method/variable
@@ -11,6 +19,14 @@ public class NumberCubeMain {
         return tosses;
     }
 
+    /** Returns the starting index of a longest run of two or more
+     * consecutive repeated values in the array values.
+     * @param values an array of integer values representing a series
+     * of number cube tosses
+     * Precondition: values.length > 0
+     * @return the starting index of a run of maximum size;
+     * -1 if there is no run
+     */
     public static int getLongestRun(int[] values) {
         int max = 0;
         int maxIndex = -1;
