9218c98e224fe6cdf99f793108176cf4a6e9124c
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-12-05 10:09:40 -0600

    Update

---
+++ src/Transaction.java
@@ -0,0 +1,15 @@
+// Daniel Szelogowski
+// 12/5/2022
+// Prog213v
+public class Transaction {
+    private String myType;
+    private double myAmount;
+
+    public Transaction(String type, double amount) {
+        myType = type;
+        myAmount = amount;
+    }
+
+    public String getCode() { return myType; }
+    public double getAmount() { return myAmount; }
+}
