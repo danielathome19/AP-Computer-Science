06dec4b187e5fca2374add98835b470877465339
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-12-05 11:23:18 -0600

    Update 12/5

---
+++ src/Q2/Prog213v/Bank.java
@@ -0,0 +1,21 @@
+package Q2.Prog213v;
+
+public class Bank {
+    private double myAmount;
+
+    public Bank(double amount) { myAmount = amount; }
+    public double getAmount()  { return myAmount; }
+
+    public void calc(Transaction thing) {
+        if (thing.getCode().equals("D")) {
+            myAmount += thing.getAmount();
+        } else if (thing.getCode().equals("S")) {
+            myAmount -= thing.getAmount();
+        } else if (thing.getCode().equals("C")) {
+            if (thing.getAmount() > this.getAmount())
+                myAmount = this.getAmount() - 10;
+            else
+                myAmount -= thing.getAmount();
+        }
+    }
+}
---
+++ src/Q2/Prog213v/Transaction.java
@@ -0,0 +1,14 @@
+package Q2.Prog213v;
+
+public class Transaction {
+    private String myType;
+    private double myAmount;
+
+    public Transaction(String type, double amount) {
+        myType = type;
+        myAmount = amount;
+    }
+
+    public String getCode()   { return myType; }
+    public double getAmount() { return myAmount; }
+}
