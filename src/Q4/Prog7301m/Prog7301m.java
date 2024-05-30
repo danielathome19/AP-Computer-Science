fd57d7a636d4ac894581c547987553a1c66c4d88
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-05-30 11:23:45 -0500

    Update 5/30

--- src/Q4/Prog7301m/Prog7301m.java
+++ src/Q4/Prog7301m/Prog7301m.java
@@ -43,6 +43,12 @@ public class Prog7301m {
         return array;
     }
 
+    public static void normalize(double[][] data, double max) {
+        for (int r = 0; r < data.length; r++)
+            for (int c = 0; c < data[r].length; c++)
+                data[r][c] /= max;
+    }
+
     public static void main(String[] args) {
         final int N_CLASSES = 10;
 
@@ -62,6 +68,9 @@ public class Prog7301m {
         double[][] y_test  = toArray(yte);
 
         final int N_INPUTS = X_train[0].length;
+        final double MAX_VAL = 255.0;
+        normalize(X_train, MAX_VAL);
+        normalize(X_test, MAX_VAL);
 
         // Create model
         var model = new MultiLayerPerceptron(new int[]{N_INPUTS, 100, N_CLASSES}, 0.1,
@@ -84,7 +93,7 @@ public class Prog7301m {
         var img = new BufferedImage(28, 28, BufferedImage.TYPE_INT_RGB);
         for (int r = 0; r < 28; r++)
             for (int c = 0; c < 28; c++)
-                img.setRGB(c, r, (int)(X_val[r * 28 + c]));
+                img.setRGB(c, r, (int)(X_val[r * 28 + c] * MAX_VAL));
 
         var frame = new JFrame("Image");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
