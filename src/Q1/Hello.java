aed14147a46787cee682a875b00d8203d7400363
Daniel J. Szelogowski <dszelogowski@gmail.com>
2024-08-29 08:50:20 -0500

    Update

--- src/Q1/Hello.java
+++
@@ -1,7 +0,0 @@
-package Q1;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Q1.Hello, world!");
-    }
-}
--- src/Q1/YourName.java
+++
@@ -1,5 +0,0 @@
-package Q1;
-
-public class YourName {
-    // Print your first and last name to the console
-}
