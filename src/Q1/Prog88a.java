54bc7eb59a80697659f00844737d07c833fcfcbf
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-09-11 11:17:53 -0500

    Update 9/11

---
+++ src/Q1/Prog88a.java
@@ -0,0 +1,37 @@
+package Q1;
+
+public class Prog88a {
+    public static void main(String[] args) {
+        // Enter number 1-13
+        // Enter number 2-20
+        // Math.random() * (max - min) + min
+        int num1 = (int) (Math.random() * (13 - 1)) + 1;
+        int num2 = (int) (Math.random() * (20 - 2)) + 2;
+        int sum = num1 + num2;
+        int dif = num1 - num2;
+        // TODO: product and average
+        int abs = Math.abs(dif);
+        int max, min;
+
+        // The code inside 'if' will only run if the condition is true
+        if (num1 > num2) {
+            max = num1;
+            // min = num2;
+        } else {  // Otherwise...
+            max = num2;
+            // min = num1;
+        }
+
+        // Check if 'max' and 'num1' are the same value using ==
+        if (max == num1) {
+            min = num2;
+        } else {
+            min = num1;
+        }
+
+        System.out.println("Original numbers are " + num1 + " and " + num2);
+        // TODO: print the rest
+        System.out.println("Maximum = " + max);
+        System.out.println("Minimum = " + min);
+    }
+}
