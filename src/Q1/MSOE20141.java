b9ccff692d4c32582ca7277beee39c7029b25ac5
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-09-25 11:00:20 -0500

    Update 9/25

---
+++ src/Q1/MSOE20141.java
@@ -0,0 +1,24 @@
+package Q1;
+import java.util.Scanner;
+
+public class MSOE20141 {
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = kbd.nextLine();
+        word = word.toLowerCase();
+        int dblcnt = 0;
+
+        for (int lcv = 0; lcv < word.length() - 1; lcv++) {
+            String cLetter = word.substring(lcv, lcv+1);    // word[lcv]
+            String nLetter = word.substring(lcv+1, lcv+2);  // word[lcv+1]
+            if (cLetter.equals(nLetter))
+                dblcnt++;
+        }
+        System.out.printf("%s contains %d double letters\n", word, dblcnt);
+    }
+}
+/*
+Enter a word: coolbeens
+coolbeens contains 2 double letters
+*/
