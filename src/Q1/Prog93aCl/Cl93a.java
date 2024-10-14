7c89d6c7d1512d40bf010b1b38821f026cdc4239
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-10-14 10:51:20 -0500

    Update 10/14

---
+++ src/Q1/Prog93aCl/Cl93a.java
@@ -0,0 +1,28 @@
+package Q1.Prog93aCl;
+
+public class Cl93a {
+    private int kwh;
+    private double baseRate;
+    private double surcharge;
+    private double cityTax;
+    private double total;
+    private double lateTotal;
+
+    public Cl93a(int kwh) {
+        this.kwh = kwh;
+        baseRate = 0;
+        surcharge = 0;
+        cityTax = 0;
+        total = 0;
+        lateTotal = 0;
+    }
+
+
+    public void calc() {}  // TODO
+
+    public String toString() {
+        return "KWH Used: " + kwh +
+               "\nBase Rate: " + baseRate +
+               "\nSurcharge: " + surcharge;  // TODO: the rest
+    }
+}
---
+++ src/Q1/Prog93aCl/Prog93aCl.java
@@ -0,0 +1,9 @@
+package Q1.Prog93aCl;
+
+public class Prog93aCl {
+    public static void main(String[] args) {
+        // TODO: use scanner for input; remove code below
+        Cl93a thing = new Cl93a(993);
+        System.out.println(thing);  // or `thing.toString()`
+    }
+}
