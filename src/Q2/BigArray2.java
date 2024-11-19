08b3c40f59ae4d4e468dfe86eb71fd9853388346
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-11-19 10:55:08 -0600

    Update 11/19

--- src/Q2/BigArray2.java
+++ src/Q2/BigArray2.java
@@ -83,11 +83,66 @@ public class BigArray2 {
             System.out.println();
 
             // 11. Remove all cats under $26. Print the costs of each cat remaining on the list.
+            int k = 0;
+            while (k < numCats) {
+                if (cats[k].getCost() < 26) {
+                    for (int j = k; j < numCats - 1; j++)
+                        cats[j] = cats[j + 1];
+                    numCats--;
+                } else {
+                    k++;
+                }
+            }
+
+            System.out.println("\n11. The cats costing >= $26 actually cost:");
+            for (int c = 0; c < numCats; c++)
+                System.out.print(cats[c].getCost() + " ");
+            System.out.println();
+
             // 12. All cats heavier than 15 pounds need to go on a diet <-- no for-each this time.
             //     Print the names of the cats being put on a diet.
+            System.out.println("\n12. The cats being put on a diet are:");
+            for (int i = 0; i < numCats; i++) {
+                Cat cat = cats[i];
+                if (cat.getWeight() > 15)
+                    System.out.print(cat.getName() + "\t");
+            }
+            System.out.println();
 
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
     }
 }
+/*
+1. All the cats:
+Name	Weight	Age	Cost
+Inky	15.690000	2	$35.79
+Panda	14.300000	6	$15.03
+Rascal	21.100000	21	$0.00
+Blacky	13.990000	3	$26.89
+Taffy	24.500000	10	$56.89
+Toby	17.200000	10	$37.57
+
+2. The third cat is named: Rascal
+3. The updated weight is: 27.2
+
+7. The updated list is:
+Name	Weight	Age	Cost
+Inky	15.690000	2	$35.79
+Angel	3.600000	1	$25.99
+Panda	14.300000	6	$15.03
+Blacky	13.990000	3	$26.89
+Taffy	24.500000	10	$56.89
+Toby	27.200000	10	$37.57
+Gimpy	14.300000	10	$29.99
+
+10. The current cat names are:
+Inky	Blacky	Sugar	Angel	Taffy	Toby	Gimpy	Panda
+
+11. The cats costing >= $26 actually cost:
+35.79 26.89 33.25 56.89 37.57 29.99
+
+12. The cats being put on a diet are:
+Inky	Sugar	Taffy	Toby
+*/
