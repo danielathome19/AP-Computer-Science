305959db162bccc73d4144017be2c96043ad3549
Daniel James Szelogowski <dszelogowski@gmail.com>
2025-01-29 10:59:09 -0600

    Update 1/29

--- src/Q3/Prog701g/Prog701g.java
+++ src/Q3/Prog701g/Prog701g.java
@@ -33,8 +33,40 @@ public class Prog701g {
                 num = file.nextInt();
             }
 
+            double tot = 0;  // for avg gpa
+            int cnt = 0;
+            int totstus = 0;  // for teachers
+            String large = "";
+            String small = "ksdjgdhkjsdgjkdsghdslkjghjjbuiewbbubivjffwfujdvjksbiuewbuiswvsjdbvksbubiwsbvibvuvkjsbdkjvbiuwbiabvakjsvbqwiubaibv";
+
+            for (Person p : list) {
+                if (p instanceof Student) {
+                    tot += ((Student) p).getGPA();
+                    cnt++;
+                } else if (p instanceof Teacher) {
+                    totstus += ((Teacher) p).getNumStus();
+                } else if (p instanceof Admin) {
+                    String word = ((Admin) p).getFavw();
+                    if (word.length() > large.length())
+                        large = word;
+                    if (word.length() < small.length())
+                        small = word;
+                }
+            }
+
+            System.out.printf("Average student GPA: %.2f\n", tot/cnt);
+            System.out.println("Total number of students taught by teachers: " + totstus);
+            System.out.println("Smallest favorite admin word: " + small);
+            System.out.println("Largest favorite admin word: " + large);
+
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
     }
 }
+/*
+Average student GPA: 3.06
+Total number of students taught by teachers: 316
+Smallest favorite admin word: done
+Largest favorite admin word: coolbeans
+*/
